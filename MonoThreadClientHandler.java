eafa194d6fd62fc2555080ece16d158be1f28f00
Mikhail Yuzhakov <miha.yuzhakov@gmail.com>
2023-03-26 15:06:18 +0300

    tcp recieve and db insert

--- MonoThreadClientHandler.java
+++ MonoThreadClientHandler.java
@@ -2,6 +2,11 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.util.Locale;
+import java.sql.*;
+import java.lang.reflect.InvocationTargetException;
 
 public class MonoThreadClientHandler implements Runnable {
 
@@ -13,13 +18,14 @@ public class MonoThreadClientHandler implements Runnable {
 
     @Override
     public void run() {
-
         try {
             DataOutputStream out = new DataOutputStream(clientDialog.getOutputStream());
             DataInputStream in = new DataInputStream(clientDialog.getInputStream());
+            Database db = new Database();
+            
 
             while (!clientDialog.isClosed()) {
-                String entry = in.readUTF();
+                db.InsertData(db.parseDate(in.readUTF()));
                 System.out.print("\nServer reading from " + clientDialog.getRemoteSocketAddress() + "> " + entry);
             }
 
@@ -29,6 +35,49 @@ public class MonoThreadClientHandler implements Runnable {
             System.out.println("Client disconnected");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+}
+
+/**
+ * Database operation
+ */
+class Database {
+	public void InsertData(JSONObject data) throws InstantiationException, IllegalAccessException, IllegalArgumentException,                 InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException {
+        String url = "jdbc:mysql://95.163.235.4:6033/cd2db?serverTimezone=Europe/Moscow&useSSL=false";
+        String username = "root";
+        String password = "passwordDB1998/";
+        Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+        try (Connection conn = DriverManager.getConnection(url, username, password);) {
+            Statement statement = conn.createStatement();
+            String cmd = String.format(Locale.ROOT, "INSERT climatic_data(probe_id, timestamp, temp_rtc, atemp, press_bmp, temp_htu, hum_htu, stemp, rssi, vacc, spc, charge) VALUES (%d, \"%s\", %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %d, %.2f, %.2f, %d)",
+                            Integer.parseInt((String) (data.get("probe_id"))), 
+                            data.get("datetime").toString(), 
+                            Float.parseFloat((String) data.get("temp_rtc")), 
+                            Float.parseFloat((String) data.get("atemp")), 
+                            Float.parseFloat((String) data.get("press_bmp")),
+                            Float.parseFloat((String) data.get("temp_htu")), 
+                            Float.parseFloat((String) data.get("hum_htu")), 
+                            Float.parseFloat((String) data.get("stemp")), 
+                            Integer.parseInt((String) (data.get("rssi"))),
+                            Float.parseFloat((String) data.get("vacc")),
+                            Float.parseFloat((String) data.get("spc")),
+                            Integer.parseInt((String) (data.get("charge"))));
+            System.out.println(cmd);
+            statement.executeUpdate(cmd);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+	public JSONObject parseDate(String data) {
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject jsonObject = (JSONObject) parser.parse(data);
+            return jsonObject;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
